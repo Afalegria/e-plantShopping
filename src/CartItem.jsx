137a931b986762a8d02f7c59c03218005e927248
Afalegria <afalegria@uc.cl>
2025-05-20 18:29:19 -0400

    segundo save con errores, debug Cartitem

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,28 +9,46 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    let totalCost = 0;
+    cart.foreach((item) => {
+        var cantidad = item.quantity
+        var costo = parseFloat(item.cost.subtstring(1))
+        totalCost += cantidad * costo
+    });
+    return totalCost;
   };
 
   const handleContinueShopping = (e) => {
-   
+      onContinueShopping(e)
   };
 
-
+  const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for future reference');
+  };
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({name: item.name, quantity: item.quantity + 1}))
   };
 
   const handleDecrement = (item) => {
-   
+    if (item.quantity > 1) { 
+        dispatch(updateQuantity({name: item.name, quantity: item.quantity - 1} ));
+    }
+    else {
+        dispatch(removeItem(item))};
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item))
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    let totalCost = item.quantity * parseFloat(item.cost.subtstring(1))
   };
+
+
+
 
   return (
     <div className="cart-container">
